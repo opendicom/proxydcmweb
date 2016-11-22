724972b11a9da2c78562504709c71c30b76ea44a
Claudio Baeza <cl.baeza@gmail.com>
2016-11-22 18:49:31 -0300

    Include rest framework swagger for documentation

--- proxydcmweb/proxydcmweb/urls.py
+++ proxydcmweb/proxydcmweb/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Proxy Dicom Web')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^proxydcmweb/', include('proxyrest.urls')),
+    url(r'^docs/$', schema_view)
 ]
